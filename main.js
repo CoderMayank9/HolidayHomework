938a062084e73744810de81530a302e239f738ff
CoderMayank9 <84262175+CoderMayank9@users.noreply.github.com>
2024-06-19 13:02:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,120 @@
+function showParagraph1() {
+    document.getElementById('paragraph-1').style.display = 'block';
+}
+
+function closeParagraph1() {
+    document.getElementById('paragraph-1').style.display = 'none';
+}
+
+window.onclick = function(event) {
+    if (event.target == document.getElementById('paragraph-1')) {
+        closeParagraph1();
+    }
+}
+
+
+
+function showParagraph2() {
+    document.getElementById('paragraph-2').style.display = 'block';
+}
+
+function closeParagraph2() {
+    document.getElementById('paragraph-2').style.display = 'none';
+}
+
+window.onclick = function(event) {
+    if (event.target == document.getElementById('paragraph-2')) {
+        closeParagraph2();
+    }
+}
+
+
+
+function showParagraph3() {
+    document.getElementById('paragraph-3').style.display = 'block';
+}
+
+function closeParagraph3() {
+    document.getElementById('paragraph-3').style.display = 'none';
+}
+
+window.onclick = function(event) {
+    if (event.target == document.getElementById('paragraph-3')) {
+        closeParagraph3();
+    }
+}
+
+
+
+function showParagraph4() {
+    document.getElementById('paragraph-4').style.display = 'block';
+}
+
+function closeParagraph4() {
+    document.getElementById('paragraph-4').style.display = 'none';
+}
+
+window.onclick = function(event) {
+    if (event.target == document.getElementById('paragraph-4')) {
+        closeParagraph4();
+    }
+}
+
+
+function showParagraph5() {
+    document.getElementById('paragraph-5').style.display = 'block';
+}
+
+function closeParagraph5() {
+    document.getElementById('paragraph-5').style.display = 'none';
+}
+
+window.onclick = function(event) {
+    if (event.target == document.getElementById('paragraph-5')) {
+        closeParagraph5();
+    }
+}
+
+
+function showParagraph6() {
+    document.getElementById('paragraph-6').style.display = 'block';
+}
+
+function closeParagraph6() {
+    document.getElementById('paragraph-6').style.display = 'none';
+}
+
+window.onclick = function(event) {
+    if (event.target == document.getElementById('paragraph-6')) {
+        closeParagraph6();
+    }
+}
+
+
+function showParagraph7() {
+    document.getElementById('paragraph-7').style.display = 'block';
+}
+
+function closeParagraph7() {
+    document.getElementById('paragraph-7').style.display = 'none';
+}
+
+window.onclick = function(event) {
+    if (event.target == document.getElementById('paragraph-7')) {
+        closeParagraph7();
+    }
+}
+
+function showParagraph8() {
+    document.getElementById('paragraph-8').style.display = 'block';
+}
+
+function closeParagraph8() {
+    document.getElementById('paragraph-8').style.display = 'none';
+}
+
+window.onclick = function(event) {
+    if (event.target == document.getElementById('paragraph-8')) {
+        closeParagraph8();
+    }
+}
